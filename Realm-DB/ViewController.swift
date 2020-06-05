eb50d39a4ff0d0bd3869399d6e16018eb03db7d0
Karim <KariimAhmed8@gmail.com>
2020-06-05 15:38:21 +0200

    Initial Commit

---
+++ Realm-DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Realm-DB
+//
+//  Created by Karem on 6/5/20.
+//  Copyright © 2020 Karem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
